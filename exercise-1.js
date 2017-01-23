d7fc7a0b78dc015dedccdbd53c59e5141afd90be
Ariel <ariel.robles@ksquareinc.com>
2017-01-22 21:58:44 -0600

    resolve second excersise

--- exercise-1.js
+++ exercise-1.js
@@ -1,9 +1,8 @@
-require('./main');
 
 //Implement an algorithm to determine if a string has all unique characters What if you can not use additional data structures?
 function findDifferentCharacters(str) {
   let uniqueCharacters = [];
-  for(let i = str.length; i >= 0; i -= 1) {
+  for(let i = str.length - 1; i >= 0; i -= 1) {
     if(uniqueCharacters.indexOf(str[i]) > -1) {
       return false;
     }
@@ -11,5 +10,5 @@ function findDifferentCharacters(str) {
   }
   return true;
 }
- console.log(findDifferentCharacters('1234567890'));
- console.log(findDifferentCharacters('1234567894'));
+ console.assert(findDifferentCharacters('1234567890'), 'should be able to check unique characters in a string');
+ console.assert(findDifferentCharacters('1234567894') == false, 'should be able to detect no unique character in a string');
